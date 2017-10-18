64b4c3e1c6ee762f0f7508e9a4118e17acf5a281
daniel <byond21@gmail.com>
2017-10-18 14:14:07 +0300

    server stuff

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,15 @@
+var mongoose = require('mongoose');
 
+var mongooseSchema = mongoose.Schema;
+
+var beerSchema = new mongooseSchema({
+    name: { type: String },
+    style: { type: String },
+    image_url: { type: String },
+    abv: { type: Number }
+
+});
+
+var Beer = mongoose.model("Beer", beerSchema);
+
+module.exports = Beer;
